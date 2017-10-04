23bf0f6ecdaa7ca66fdeb2d9de9eabcd58259484
Frank Schoenmann <fschoenm@gmail.com>
2017-10-04 22:31:20 +0200

    Initial Commit

---
+++ WeatherBar/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  WeatherBar
+//
+//  Created by Frank Schönmann on 04.10.17.
+//  Copyright © 2017 Frank Schönmann. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	@IBOutlet weak var window: NSWindow!
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
